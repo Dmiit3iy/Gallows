8829c43dbba520151a8aef62cbedf0ce1c356c1b
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2024-12-26 17:42:04 +0500

    первый  финальный вариант (fix User: add @Getter)

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,10 +1,11 @@
 package model;
 
 import constant.Constants;
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-
+@Getter
 @RequiredArgsConstructor
 public class User {
     @NonNull
